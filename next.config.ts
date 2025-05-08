5429db58ab14a263e37fcf1dc30b3547bb4df555
Hiroaki0117 <76091609+Hiroaki0117@users.noreply.github.com>
2025-05-08 07:35:37 +0000

    サインアウトができない問題を修正

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      // 本番環境なら your-domain.com などを追加
+      allowedOrigins:       ['localhost:3000'],
+      allowedForwardedHosts: ['localhost:3000'],
+    }
+  }
 };
 
 export default nextConfig;
